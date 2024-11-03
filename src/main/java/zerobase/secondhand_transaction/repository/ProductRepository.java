1e0753e0c8a6bc1e6e10013d873a30cc25b4f431
jiwookwon0812 <121539868+jiwookwon0812@users.noreply.github.com>
2024-11-04 02:47:42 +0900

    Delete src directory

--- src/main/java/zerobase/secondhand_transaction/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package zerobase.secondhand_transaction.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import zerobase.secondhand_transaction.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
--- src/main/java/zerobase/secondhand_transaction/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package zerobase.secondhand_transaction.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class ProductService {
-}
